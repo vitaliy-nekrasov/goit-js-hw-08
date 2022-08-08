ab5346694f81cccd9a73feb11ba586e4c319af90
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-08-08 15:14:07 +0300

    Done task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('form');
+const obj = {
+  email: '',
+  message: '',
+};
+
+formEl.addEventListener('input', throttle(onTextInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onTextInput(evt) {
+  const email = evt.currentTarget.email.value;
+  const message = evt.currentTarget.message.value;
+
+  obj.email = email;
+  obj.message = message;
+
+  const objJSON = JSON.stringify(obj);
+  localStorage.setItem('feedback-form-state', objJSON);
+}
+
+if (localStorage.getItem('feedback-form-state')) {
+  const { email, message } = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  formEl.email.value = email;
+  formEl.message.value = message;
+
+  obj.email = email;
+  obj.message = message;
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+
+  console.log(obj);
+
+  localStorage.removeItem('feedback-form-state');
+}
