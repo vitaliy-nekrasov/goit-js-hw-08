ce78e1dce17b4df48ffc63125380bd1866b1ac3d
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-08-08 11:10:55 +0300

    Done task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(evt) {
+  const currentTime = evt;
+  const currentTimeJSON = JSON.stringify(currentTime);
+  localStorage.setItem('videoplayer-current-time', currentTimeJSON);
+}
+
+const getTimeJSON = localStorage.getItem('videoplayer-current-time');
+const getTime = JSON.parse(getTimeJSON).seconds;
+
+player.setCurrentTime(getTime);
